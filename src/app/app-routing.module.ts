b36dec0a2dca37de5c0fa7c85ed70a65332f0750
dianaciro <kathe9463@gmail.com>
2017-12-03 20:14:12 -0500

    Creación componente vehicle

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+
+import { NgModule } from '@angular/core';
+
+const routes: Routes= [
+  {path: '', redirectTo: '/appComponent', pathMatch:'full'},
+  {path : 'appComponent', component: AppComponent}
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule { }
+
